26bf1d7ef057134817e046884d95137f441da19f
thehambeard <61787444+thehambeard@users.noreply.github.com>
2024-12-05 00:41:18 -0500

    Fix save exception.

--- Settings.cs
+++ Settings.cs
@@ -5,5 +5,7 @@ namespace DisableCSO
     public class Settings : UnityModManager.ModSettings
     {
         public bool LetterBox = true;
+
+        public override void Save(UnityModManager.ModEntry modEntry) => base.Save(modEntry);
     }
 }
